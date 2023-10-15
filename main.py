a51cfc5289ae61be94704e3362f6da03e88e5f66
kadarkojr <kwabenaamoakodarko47@gmail.com>
2023-10-15 03:31:30 +0000

    changes to main

--- main.py
+++ main.py
@@ -129,3 +129,7 @@ app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(vote.router)
+
+@app.get('/')
+def getter():
+    return {"message" : "hello world"}
