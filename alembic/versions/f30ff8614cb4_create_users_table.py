582088ffc8ff92e4ece852a5165dc7e8be309fe5
kadarkojr <kwabenaamoakodarko47@gmail.com>
2023-10-16 12:21:01 +0000

    changes to alembic

--- alembic/versions/f30ff8614cb4_create_users_table.py
+++ alembic/versions/f30ff8614cb4_create_users_table.py
@@ -22,7 +22,7 @@ def upgrade():
     op.create_table('users', sa.Column('id', sa.Integer(), autoincrement=True, nullable=False),
     sa.Column('email', sa.String(), nullable=False),
     sa.Column('password', sa.String, nullable=False),
-    sa.Column('createdAt', sa.TIMESTAMP(timezone=True), server_default=sa.text('now()'), nullable=False),
+    sa.Column('created_at', sa.TIMESTAMP(timezone=True), server_default=sa.text('now()'), nullable=False),
     sa.PrimaryKeyConstraint('id'),
     sa.UniqueConstraint('email'))
     pass
